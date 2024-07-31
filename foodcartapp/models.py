d7c961431dd1768d4c3ad2a28344ef3a1b72f51b
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-07-31 20:01:30 +0500

    add time to orders

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import F, Sum
 from django.core.validators import MinValueValidator
+from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -166,6 +167,23 @@ class Order(models.Model):
     comment = models.TextField(
         'Комментарий',
         blank=True,
+    )
+    registered_at = models.DateTimeField(
+        'Дата регистрации',
+        default=timezone.now,
+        db_index=True,
+    )
+    called_at = models.DateTimeField(
+        'Дата звонка',
+        null=True,
+        blank=True,
+        db_index=True,
+    )
+    delivered_at = models.DateTimeField(
+        'Дата доставки',
+        null=True,
+        blank=True,
+        db_index=True,
     )
     objects = OrderQuerySet.as_manager()
 
