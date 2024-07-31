d2e62eed77a2d3f6043bd59d774d3b54ec45a92a
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-07-31 19:53:29 +0500

    redict for orders

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import reverse, redirect
 from django.templatetags.static import static
+from django.utils.encoding import iri_to_uri
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product
 from .models import ProductCategory
@@ -118,3 +120,12 @@ class OrderAdmin(admin.ModelAdmin):
     inlines = [
         OrderElementInline
     ]
+
+    def response_change(self, request, obj):
+        response = super(OrderAdmin, self).response_change(request, obj)
+        if 'next' in request.GET:
+            if url_has_allowed_host_and_scheme(request.GET['next'], None):
+                url = iri_to_uri(request.GET['next'])
+                return redirect(url)
+        else:
+            return response
